abc00599d3f7780a4ba7e16984bd53d6252af6cb
ImAbobaBoy <sysoev.ae705@gmail.com>
2024-04-03 23:29:47 +0500

    переписал апп в функцию

--- src/index.js
+++ src/index.js
@@ -1,63 +1,63 @@
-import React from "react"
-import * as ReactDOMClient from "react-dom/client"
-import './css/style.css'
-import logo from './img/LogoPlaceholder.png'
-import notification from './img/NotificationButton.svg'
-import avatar from './img/avatarPlaceholder.jpg'
-import Frames from './Components/frames.js'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import './css/style.css';
+import logo from './img/LogoPlaceholder.png';
+import notification from './img/NotificationButton.svg';
+import avatar from './img/avatarPlaceholder.jpg';
+import Frames from './Components/frames.js';
+import { Link, useSearchParams } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Profile from './Pages/profile'
-import Main from './Pages/main'
+import Profile from './Pages/profile';
+import Main from './Pages/main';
 
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-        frames: [
-        ],
-        isLoggedIn: false
-    }
+function App() {
+ const [frames, setFrames] = useState([]);
+ const [isLoggedIn, setIsLoggedIn] = useState(false);
+ const [users, setUsers] = useState([]);
 
-    this.AddFrame = this.AddFrame.bind(this)
-    this.MakeLoggedIn = this.MakeLoggedIn.bind(this)
-  }
-  render () {
-    return (
-      <Router>
-        <>
-            <header className="header">
-              <Link to="/" className = "Logo"><img src={logo} width={200} height={69} /></Link>
-              <a href="#"><p className = "PublishProject"> Опубликовать проект</p></a>
-              <a href="#" className = "Notification"><img className = "Notification" src={notification} width={45} /></a>
-              <Link to="/profile" className="Avatar">
-                <img className="Avatar" src={avatar} width={90} />
-              </Link>
-            </header>
-            {this.state.isLoggedIn 
-            ? (<p>Вы молодец</p>) : (<p>Зарегестрируйтесь пожалуйста или войдите в аккаунт</p>)}  
-            <Routes>
-              <Route exact path="/" element={<Main frames={this.state.frames} onAdd={this.AddFrame}/>} />
-              <Route exact path="/profile" element={<Profile frames={this.state.frames} onLogIn={this.MakeLoggedIn} onAdd={this.AddFrame}/>} />
-            </Routes>
-        </>
-        </Router>
-    )
-  }
+ useEffect(() => {
+  fetchUsers()
+ }, []);
+ 
+ const fetchUsers = async () => {
+  const response = await fetch("https://127.0.0.1:5500/users")
+  const data = await response.json()
+  setUsers(data.users)
+  console.log(data.users)
+ }
 
-  AddFrame(frame){
-    const id = this.state.frames.length + 1
-    const specialization = "deafault"
-    const isLookinForTeam = true
+ const addFrame = (frame) => {
+    const id = frames.length + 1;
+    const specialization = "default";
+    const isLookingForTeam = true;
 
-    this.setState({frames: [...this.state.frames, {id, ...frame, specialization, isLookinForTeam }]})
-  }
+    setFrames([...frames, {id, ...frame, specialization, isLookingForTeam }]);
+ };
+  
+ const makeLoggedIn = () => {
+    setIsLoggedIn(true);
+ };
 
-  MakeLoggedIn(){
-    this.setState({isLoggedIn: true})
-  }
-} 
+ return (
+    <Router>
+      <>
+        <header className="header">
+          <Link to="/" className="Logo"><img src={logo} width={200} height={69} /></Link>
+          <a href="#"><p className="PublishProject">Опубликовать проект</p></a>
+          <a href="#" className="Notification"><img className="Notification" src={notification} width={45} /></a>
+          <Link to="/profile" className="Avatar">
+            <img className="Avatar" src={avatar} width={90} />
+          </Link>
+        </header>
+        {isLoggedIn 
+        ? (<p>Вы молодец</p>) : (<p>Зарегистрируйтесь пожалуйста или войдите в аккаунт</p>)}  
+        <Routes>
+          <Route exact path="/" element={<Main frames={frames} onAdd={addFrame}/>} />
+          <Route exact path="/profile" element={<Profile frames={frames} onLogIn={makeLoggedIn} onAdd={addFrame}/>} />
+        </Routes>
+      </>
+    </Router>
+ );
+}
 
-const app = ReactDOMClient.createRoot(document.getElementById("app"))
-
-app.render(<App />)
+ReactDOM.render(<App />, document.getElementById("app"));
