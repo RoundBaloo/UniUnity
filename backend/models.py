0fbab46175370495c809e5305c8f431efd1fa51e
ubivashka228 <164536326+ubivashka228@users.noreply.github.com>
2024-04-07 18:49:46 +0500

    регистрация и авторизация, работа с проектом
    Добавил регистрацию и авторизацию, получение проектов пользователя, добавление, изменение и удаление проекта пользователем

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,12 @@
 from config import db
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
+from passlib.hash import bcrypt
 
 
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
 
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -18,6 +23,24 @@ class User(db.Model):
     about = db.Column(db.Text())
     skill_level = db.Column(db.String(100), nullable=False)
     team_search_state = db.Column(db.String(100), nullable=False)
+    projects = db.relationship('Project', backref='user', lazy=True)
+
+    def __init__(self, **kwargs):
+        self.email = kwargs.get('email')
+        self.password = bcrypt.hash(kwargs.get('password'))
+
+    def get_token(self, expire_time=24):
+        expire_delta = timedelta(expire_time)
+        token = create_access_token(
+            identity=self.id, expires_delta=expire_delta)
+        return token
+
+    @classmethod
+    def authenticate(cls, email, password):
+        user = cls.query.filter(cls.email == email).one()
+        if not bcrypt.verify(password, user.password):
+            raise Exception('Incorrect password')
+        return user
 
     def to_json(self):
         return {
@@ -35,5 +58,24 @@ class User(db.Model):
             "searchAim": self.search_aim,
             "about": self.about,
             "skillLevel": self.skill_level,
-            "teamSearchState": self.team_search_state
+            "teamSearchState": self.team_search_state,
+            "projects": self.projects
+        }
+
+
+class Project(db.Model):
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    type = db.Column(db.String(70), nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "type": self.type,
+            "description": self.description
         }
