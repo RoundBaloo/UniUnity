9cf157648cdba201f3412056b1f4be68c90a4720
ubivashka228 <164536326+ubivashka228@users.noreply.github.com>
2024-04-14 19:08:03 +0500

    update error catching
    add error catching for all routes and add log for them

--- backend/config.py
+++ backend/config.py
@@ -5,10 +5,14 @@ from flask_jwt_extended import JWTManager
 from apispec.ext.marshmallow import MarshmallowPlugin
 from apispec import APISpec
 from flask_apispec.extension import FlaskApiSpec
+import logging
 
 app = Flask(__name__)
+
 CORS(app)
+
 jwt = JWTManager(app)
+
 docs = FlaskApiSpec()
 docs.init_app(app)
 app.config.update({
@@ -20,6 +24,22 @@ app.config.update({
     ),
     'APISPEC_SWAGGER_URL': '/swagger/'
 })
+
+
+def setup_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+    file_handler = logging.FileHandler('log/app.log')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+logger = setup_logger()
+
 
 app.config['SECRET_KEY'] = '4bc3c1f05653443ca31d24b89dbedb90'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@
 from flask import jsonify, request
 from flask_cors import CORS
 from flask_jwt_extended import jwt_required, get_jwt_identity, verify_jwt_in_request
-from config import app, db, docs
+from config import app, db, docs, logger
 from models import User, Project
 from schemas import ProjectSchema
 from flask_apispec import use_kwargs, marshal_with
@@ -13,8 +13,13 @@ CORS(app)
 # Получение юзеров
 @app.route("/users", methods=["GET"])
 def get_users():
-    users = User.query.all()
-    json_users = list(map(lambda x: x.to_json(), users))
+    try:
+        users = User.query.all()
+        json_users = list(map(lambda x: x.to_json(), users))
+    except Exception as e:
+        logger.warning(f'Error while getting users: {e}')
+        return jsonify({"message": str(e)}), 400
+
     return jsonify({"users": json_users}), 200
 
 
@@ -22,27 +27,36 @@ def get_users():
 @app.route("/get_user_id", methods=["GET"])
 @jwt_required()
 def get_user_id():
-    user_id = get_jwt_identity()
+    try:
+        user_id = get_jwt_identity()
+    except Exception as e:
+        logger.warning(f'Error while getting user_id:{get_jwt_identity()}: {e}')
+        return jsonify({"message": str(e)}), 401
+
     return jsonify({"user_id": user_id}), 200
 
 
 # Обновление юзера
 @app.route("/update_user", methods=["PATCH"])
 def update_user():
-    verify_jwt_in_request()
-    user_id = get_jwt_identity()
-    user = User.query.get(user_id)
+    try:
+        verify_jwt_in_request()
+        user_id = get_jwt_identity()
+        user = User.query.get(user_id)
 
-    # Существует ли юзер
-    if not user:
-        return jsonify({"message": "Пользователь не найден"}), 404
+        # Существует ли юзер
+        if not user:
+            logger.warning(f'Пользователь с id: {user_id} не найден ')
+            return jsonify({"message": "Пользователь не найден"}), 404
 
-    data = request.json  # Словарь
+        data = request.json  # Словарь
+        for key, value in data.items():
+            setattr(user, key, value)
 
-    for key, value in data.items():
-        setattr(user, key, value)
-
-    db.session.commit()
+        db.session.commit()
+    except Exception as e:
+        logger.warning(f'Error while updating user: {get_jwt_identity()}: {e}')
+        return jsonify({"message": str(e)}), 401
 
     return jsonify({"message": "Данные о пользователе обновлены"}), 201
 
@@ -50,14 +64,19 @@ def update_user():
 # Удаление юзера
 @app.route("/delete_user>", methods=["DELETE"])
 def delete_user():
-    user_id = get_jwt_identity()
-    user = User.query.get(user_id)
+    try:
+        user_id = get_jwt_identity()
+        user = User.query.get(user_id)
 
-    if not user:
-        return jsonify({"message": "Пользователь не найден"}), 404
+        if not user:
+            logger.warning(f'Пользователь с id: {user_id} не найден ')
+            return jsonify({"message": "Пользователь не найден"}), 404
 
-    db.session.delete(user)
-    db.session.commit()
+        db.session.delete(user)
+        db.session.commit()
+    except Exception as e:
+        logger.warning(f'Error while deleting user {get_jwt_identity()}: {e}')
+        return jsonify({"message": str(e)}), 401
 
     return jsonify({"message": "Пользователь удален"}), 200
 
@@ -65,27 +84,31 @@ def delete_user():
 # Регистрация
 @app.route("/register", methods=["POST"])
 def register():
-    params = request.json
-    user = User(**params)
     try:
-        print(1)
+        params = request.json
+        user = User(**params)
+
         db.session.add(user)  # подготовка к добавлению в БД
-        print(2)
         db.session.commit()  # добавление в БД
-        print(3)
+        token = user.get_token()
     except Exception as e:
+        logger.warning(f'Error while registering user: {e}')
         return jsonify({"message": str(e)}), 400
 
-    token = user.get_token()
     return {'access_token': token}  # доступ к аккаунту
 
 
 # Вход в аккаунт
 @app.route("/login", methods=["POST"])
 def login():
-    params = request.json
-    user = User.authenticate(**params)
-    token = user.get_token()
+    try:
+        params = request.json
+        user = User.authenticate(**params)
+        token = user.get_token()
+    except Exception as e:
+        logger.warning(f'Error while login {request.json.get("email")}: {e}')
+        return jsonify({"message": str(e)}), 400
+
     return {'access_token': token}  # доступ к аккаунту
 
 
@@ -93,7 +116,12 @@ def login():
 @app.route("/get_user_projects/<int:user_id>", methods=["GET"])
 @marshal_with(ProjectSchema(many=True))
 def get_projects(user_id):
-    projects = Project.query.filter(Project.user_id == user_id)
+    try:
+        projects = Project.query.filter(Project.user_id == user_id)
+    except Exception as e:
+        logger.warning(f'Error while getting projects {user_id}: {e}')
+        return jsonify({"message": str(e)}), 404
+
     return projects, 200
 
 
@@ -103,13 +131,15 @@ def get_projects(user_id):
 @use_kwargs(ProjectSchema)
 @marshal_with(ProjectSchema)
 def post_project(**kwargs):
-    user_id = get_jwt_identity()
-    new_one = Project(user_id=user_id, **kwargs)
     try:
+        user_id = get_jwt_identity()
+        new_one = Project(user_id=user_id, **kwargs)
         db.session.add(new_one)  # подготовка к добавлению в БД
         db.session.commit()  # добавление в БД
     except Exception as e:
+        logger.warning(f'Error while posting project {get_jwt_identity()}: {e}')
         return jsonify({"message": str(e)}), 400
+
     return new_one, 201
 
 
@@ -119,20 +149,25 @@ def post_project(**kwargs):
 @use_kwargs(ProjectSchema)
 @marshal_with(ProjectSchema)
 def update_project(project_id, **kwargs):
-    user_id = get_jwt_identity()
-    project = Project.query.filter(
-        Project.user_id == user_id,
-        Project.id == project_id
-    ).first()
+    try:
+        user_id = get_jwt_identity()
+        project = Project.query.filter(
+            Project.user_id == user_id,
+            Project.id == project_id).first()
 
-    # Существует ли проект
-    if not project:
-        return jsonify({"message": "Проект не найден или вы не имеете к нему доступа"}), 404
+        # Существует ли проект
+        if not project:
+            logger.warning(f'Проект с user_id: {user_id} и id: {project_id} не найден ')
+            return jsonify({"message": "Проект не найден или вы не имеете к нему доступа"}), 404
 
-    for key, value in kwargs.items():
-        setattr(project, key, value)
+        for key, value in kwargs.items():
+            setattr(project, key, value)
+        db.session.commit()
+    except Exception as e:
+        logger.warning(
+            f'Error while updating project user:{get_jwt_identity()} project:{project_id}: {e}')
+        return jsonify({"message": str(e)}), 401
 
-    db.session.commit()
     return project, 201
 
 
@@ -140,19 +175,34 @@ def update_project(project_id, **kwargs):
 @app.route("/delete_project/<int:project_id>", methods=["DELETE"])
 @jwt_required()
 def delete_project(project_id):
-    user_id = get_jwt_identity()
-    project = Project.query.filter(
-        Project.user_id == user_id,
-        Project.id == project_id
-    ).first()
+    try:
+        user_id = get_jwt_identity()
+        project = Project.query.filter(
+            Project.user_id == user_id,
+            Project.id == project_id).first()
 
-    if not project:
-        return jsonify({"message": "Проект не найден или вы не имеете к нему доступа"}), 404
+        if not project:
+            logger.warning(f'Проект с user_id: {user_id} и id: {project_id} не найден ')
+            return jsonify({"message": "Проект не найден или вы не имеете к нему доступа"}), 404
 
-    db.session.delete(project)
-    db.session.commit()
+        db.session.delete(project)
+        db.session.commit()
+    except Exception as e:
+        logger.warning(
+            f'Error while deleting project user:{get_jwt_identity()} project:{project_id}: {e}')
+        return jsonify({"message": str(e)}), 401
 
     return jsonify({"message": "Проект удален"}), 200
+
+
+@app.errorhandler(422)
+def error_handlesr(err):
+    headers = err.data.get("headers", None)
+    messages = err.data.get("messages", ["Invalid Request"])
+    if headers:
+        return jsonify({"message": messages}), 400, headers
+    else:
+        return jsonify({"message": messages}), 400
 
 
 docs.register(get_projects)
