69a035202c29f8d0a29847f31791609fd94f3ae8
onepart1cle <145687201+onepart1cle@users.noreply.github.com>
2024-06-11 00:23:25 +0500

    some style

--- src/Pages/main.js
+++ src/Pages/main.js
@@ -4,6 +4,8 @@ import Frames from '../Components/frames.js'
 import NewUserAgitation from '../Components/newUserAgitation.js'
 import {useNavigate} from 'react-router-dom';
 import styled from 'styled-components';
+import {motion, useScroll, useMotionValueEvent} from "framer-motion";
+
 
 
 const StyledTurningPagesButtonsContainer = styled.div`
@@ -19,7 +21,7 @@ const StyledTurningPagesButtonsContainer = styled.div`
 const FilterContainer = styled.div`
     text-align: left;
     height: 440px;
-    width: 330px;
+    width: 372px;
     border: 3px solid black;
     border-radius: 23px;
     margin-top: 44px;
@@ -47,6 +49,15 @@ const StyledTurningButton = styled.button`
     }
 `;
 
+const LeftColumn = styled.div`
+    border-right: 2px;
+    width: 143px;
+`;
+
+const RightColumn = styled.div`
+    
+`;
+
 const Main = (props) => {
     const [instituteFilter, setInstituteFilter] = useState('');
     const [studyDirectionFilter, setStudyDirectionFilter] = useState('');
@@ -54,6 +65,18 @@ const Main = (props) => {
     const [professionFilter, setprofessionFilter] = useState('');
     const [skillLevelFilter, setSkillLevelFilter] = useState('');
     const [teamSearchStateFilter, setTeamSearchStateFilter] = useState();
+
+    const { scrollY } = useScroll();
+    const [hidden, setHidden] = useState(false);
+    useMotionValueEvent(scrollY, "change", (latest) => {
+        const previous = scrollY.getPrevious();
+        if (latest > previous && latest > 150) {
+            setHidden(true);
+        }
+        else {
+            setHidden(false);
+        }
+    });
 
     const navigate = useNavigate();
 
@@ -63,39 +86,108 @@ const Main = (props) => {
 
     return (
         <>
-            {!props.onLogIn && <NewUserAgitation onGoToin={goToIn} onMakeRegistration={props.onMakeRegistration}
-                                                 onMakeNonRegistration={props.onMakeNonRegistration}/>}
+            <motion.div variants={{
+                hidden: {y:"-100%"},
+                visible: {y:0},
+            }}
+            animate={hidden ? "hidden" : "visible"}
+            transition={{duration: 0.35, ease: "easeInOut"}}>
+                {!props.onLogIn && <NewUserAgitation onGoToin={goToIn} onMakeRegistration={props.onMakeRegistration}
+                                                     onMakeNonRegistration={props.onMakeNonRegistration}/>}
+            </motion.div>
+
             <FilterContainer>
                 <FilterButtonP>Фильтр и поиск</FilterButtonP>
-                <select onChange={(e) => {setCourseFilter(e.target.value)}}>курс
+                <LeftColumn>
+                    <ul>
+                        <li>Курс</li>
+                        <li>Институт</li>
+                        <li>Направление</li>
+                        <li>Профессия</li>
+                        <li>Поиск команды</li>
+                    </ul>
+                </LeftColumn>
+
+                <RightColumn>
+                    <ul>
+                        <li>
+                            <select onChange={(e) => {
+                                setCourseFilter(e.target.value)
+                            }}>курс
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                            </select>
+                        </li>
+                        <li><input placeholder="input field" onChange={(e) => {
+                            setInstituteFilter(e.target.value)
+                        }}></input></li>
+                        <li>
+                            <input placeholder="input field" onChange={(e) => {
+                            setStudyDirectionFilter(e.target.value)}}>
+                            </input>
+                        </li>
+                        <li>
+                            <input placeholder="input field" onChange={(e) => {
+                            setprofessionFilter(e.target.value)
+                        }}></input>
+                        </li>
+                        <li>
+                            <input
+                            type="checkbox"
+                            onChange={(e) => {
+                                setTeamSearchStateFilter(e.target.checked)
+                            }}/>
+                        </li>
+                    </ul>
+                </RightColumn>
+                <select onChange={(e) => {
+                    setCourseFilter(e.target.value)
+                }}>курс
                     <option>1</option>
                     <option>2</option>
                     <option>3</option>
-                    <option>4</option>  
+                    <option>4</option>
                 </select>
                 <p>институт</p>
-                <input placeholder="input field" onChange={(e) => {setInstituteFilter(e.target.value)}}></input>
+                <input placeholder="input field" onChange={(e) => {
+                    setInstituteFilter(e.target.value)
+                }}></input>
                 <p>направление обучения</p>
-                <input placeholder="input field" onChange={(e) => {setStudyDirectionFilter(e.target.value)}}></input>
+                <input placeholder="input field" onChange={(e) => {
+                    setStudyDirectionFilter(e.target.value)
+                }}></input>
                 <p>профессия</p>
-                <input placeholder="input field" onChange={(e) => {setprofessionFilter(e.target.value)}}></input>
+                <input placeholder="input field" onChange={(e) => {
+                    setprofessionFilter(e.target.value)
+                }}></input>
                 <p>скилл</p>
-                <input placeholder="input field" onChange={(e) => {setSkillLevelFilter(e.target.value)}}></input>
+                <input placeholder="input field" onChange={(e) => {
+                    setSkillLevelFilter(e.target.value)
+                }}></input>
                 <p>ищет команду?</p>
                 <input
                     type="checkbox"
-                    onChange={(e) => {setTeamSearchStateFilter(e.target.checked)}}
+                    onChange={(e) => {
+                        setTeamSearchStateFilter(e.target.checked)
+                    }}
                 />
-                <button type='button' onClick={(e) => {props.updateUsersByFilters({
-                    institute: instituteFilter,
-                    studyDirection: studyDirectionFilter,
-                    course: courseFilter,
-                    profession: professionFilter,
-                    skillLevel: skillLevelFilter,
-                    teamSearchState: teamSearchStateFilter
-                })}}>сохранить фильтры</button>
+
+                <button type='button' onClick={(e) => {
+                    props.updateUsersByFilters({
+                        institute: instituteFilter,
+                        studyDirection: studyDirectionFilter,
+                        course: courseFilter,
+                        profession: professionFilter,
+                        skillLevel: skillLevelFilter,
+                        teamSearchState: teamSearchStateFilter
+                    })
+                }}>сохранить фильтры
+                </button>
             </FilterContainer>
-            <Frames frames={props.frames} updateUserId={props.updateUserId} onUpdateThisFrame={props.onUpdateThisFrame} updateThisFrameToOther={props.updateThisFrameToOther}/>
+            <Frames frames={props.frames} updateUserId={props.updateUserId} onUpdateThisFrame={props.onUpdateThisFrame}
+                    updateThisFrameToOther={props.updateThisFrameToOther}/>
             <StyledTurningPagesButtonsContainer>
                 <StyledTurningButton type='button' onClick={() => {
                     props.scrollBack();
