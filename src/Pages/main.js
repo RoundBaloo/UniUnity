16c3771481baab85d7c7e157d5be0d5d696b9a1e
onepart1cle <145687201+onepart1cle@users.noreply.github.com>
2024-06-11 19:24:44 +0500

    filter styles

--- src/Pages/main.js
+++ src/Pages/main.js
@@ -19,9 +19,10 @@ const StyledTurningPagesButtonsContainer = styled.div`
 `;
 
 const FilterContainer = styled.div`
+    position: absolute;
+    top: -94px;
     text-align: left;
-    height: 740px;
-    width: 372px;
+    width: 420px;
     border: 3px solid black;
     border-radius: 23px;
     margin-top: 44px;
@@ -29,13 +30,26 @@ const FilterContainer = styled.div`
     margin-bottom: 100px;
     cursor: pointer;
     overflow: hidden;
+    background: rgba(255, 255, 255, 0.75);
+    backdrop-filter: blur(5px);
+    z-index: 1;
+    transition: height 2s;
+`;
+
+const FilterContainer2 = styled.div`
+    display: flex;
 `;
 
 const FilterButtonP = styled.div`
     font-size: 18px;
-    padding-left: 40px;
-    padding-top: 9px;
     margin:0;
+    height: 50px;
+    width: 360px;
+    border: 3px solid black;
+    border-radius: 31px;
+    margin-left: 65px;
+    padding-left: 40px;
+    padding-top: 12px;
 `;
 
 const StyledTurningButton = styled.button`
@@ -50,12 +64,64 @@ const StyledTurningButton = styled.button`
 `;
 
 const LeftColumn = styled.div`
-    border-right: 2px;
-    width: 143px;
+    padding: 25px;
+    padding-bottom: 0;
+    border-right: 3px solid black;
 `;
 
 const RightColumn = styled.div`
-    
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-top: 18px;
+    padding-bottom: 0;
+`;
+
+const StyledUlLeft = styled.ul`
+    list-style-type: none;
+    font-weight: bold;
+    & > li {
+        font-size: 20px;
+        line-height: 17.5px;
+        margin-bottom: 34px;
+    }
+`;
+
+const StyledUlRight = styled.ul`
+    list-style-type: none;
+    font-weight: bold;
+    & > li {
+        display: flex;
+        align-items: center;
+        margin-bottom: 20px;
+
+        font-size: 18px;
+        line-height: 15px;
+        border: 2px solid black;
+        border-radius: 15px;
+        width: 200px;
+        height: 32px;
+        padding-left: 10px
+    }
+`;
+
+const FilterWrapper = styled.div`
+    position: relative;
+    width: 400px;
+`;
+
+const SaveFiltersButton = styled.button`
+    width: 100%; 
+    position: relative;
+    border-top: 3px solid black;
+    text-align: left;
+    padding: 18px;
+    padding-left: 25px;
+    font-size: 20px;
+
+    &:hover {
+        background-color: black;
+        color: white;
+    }
 `;
 
 const Main = (props) => {
@@ -78,6 +144,9 @@ const Main = (props) => {
         }
     });
 
+    const [isFilterVisible, setIsFilterVisible] = useState(false); // Новое состояние
+
+
     const navigate = useNavigate();
 
     const goToIn = () => {
@@ -95,12 +164,81 @@ const Main = (props) => {
                 {!props.onLogIn && <NewUserAgitation onGoToin={goToIn} onMakeRegistration={props.onMakeRegistration}
                                                      onMakeNonRegistration={props.onMakeNonRegistration}/>}
             </motion.div>
+            <FilterWrapper>
+                {isFilterVisible ? (
+                    <FilterContainer>
+                        <FilterContainer2>
+                            <LeftColumn>
+                                <StyledUlLeft>
+                                    <li>Курс</li>
+                                    <li>Институт</li>
+                                    <li>Направление</li>
+                                    <li>Профессия</li>
+                                    <li>Поиск команды</li>
+                                </StyledUlLeft>
+                            </LeftColumn>
+                            <RightColumn>
+                                <StyledUlRight>
+                                    <li>
+                                        <select onChange={(e) => {
+                                            setCourseFilter(e.target.value)
+                                        }}>курс
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                        </select>
+                                    </li>
+                                    <li><input placeholder="" onChange={(e) => {
+                                        setInstituteFilter(e.target.value)
+                                    }}></input></li>
+                                    <li>
+                                        <input placeholder="" onChange={(e) => {
+                                            setStudyDirectionFilter(e.target.value)
+                                        }}>
+                                        </input>
+                                    </li>
+                                    <li>
+                                        <input placeholder="" onChange={(e) => {
+                                            setprofessionFilter(e.target.value)
+                                        }}></input>
+                                    </li>
+                                    <li>
+                                        <input
+                                            type="checkbox"
+                                            onChange={(e) => {
+                                                setTeamSearchStateFilter(e.target.checked)
+                                            }}/>
+                                    </li>
+                                </StyledUlRight>
+                            </RightColumn>
 
-            <FilterContainer>
+                        </FilterContainer2>
+
+                        <SaveFiltersButton type='button' onClick={(e) => {
+                            props.updateFilters({
+                                institute: instituteFilter,
+                                studyDirection: studyDirectionFilter,
+                                course: courseFilter,
+                                profession: professionFilter,
+                                skillLevel: skillLevelFilter,
+                                teamSearchState: teamSearchStateFilter
+                            });
+                            setIsFilterVisible(false);
+                        }}>Сохранить фильтры
+                        </SaveFiltersButton>
+                    </FilterContainer>
+                ) : (
+                    <FilterButtonP onClick={() => setIsFilterVisible(true)}>Фильтр и поиск</FilterButtonP>
+                )}
+            </FilterWrapper>
+
+
+            {/*<FilterContainer>
                 <FilterButtonP>Фильтр и поиск</FilterButtonP>
                 <LeftColumn>
                     <ul>
-                        <li>Курс</li>
+                        <--!dfghgh!--><li>Курс</li>
                         <li>Институт</li>
                         <li>Направление</li>
                         <li>Профессия</li>
@@ -185,7 +323,9 @@ const Main = (props) => {
                     })
                 }}>сохранить фильтры
                 </button>
-            </FilterContainer>
+            </FilterContainer>*/}
+
+
             <Frames frames={props.frames} updateUserId={props.updateUserId} onUpdateThisFrame={props.onUpdateThisFrame}
                     updateThisFrameToOther={props.updateThisFrameToOther}/>
             <StyledTurningPagesButtonsContainer>
